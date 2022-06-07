424e08b3ff1de3cc8d50c0282795954bfb6dbfc8
Aaliya <Aaliya@172.16.11.41>
2022-06-07 16:00:15 -0700

    eighth commit

---
+++ Access Modifiers/src/demo1/test3.java
@@ -0,0 +1,16 @@
+package demo1;
+ import AccessModifiersDemo.test1;
+
+public class test3 extends test1{
+
+	public static void main(String[] args) {
+		test3 t3=new test3();
+		t3.show();
+		System.out.println(t3.i);
+	}
+}
+	
+
+	
+
+
