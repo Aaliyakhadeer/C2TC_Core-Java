8f79367958499202e0f2a94fe08dc125c6e79890
Aaliya <Aaliya@DESKTOP-3DUR69C>
2022-06-16 16:04:57 -0700

    13 commit

---
+++ static/src/Demo.java
@@ -0,0 +1,13 @@
+
+public class Demo {
+	static 
+	{
+		System.out.println("static block is invoked");
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.println("hello main");
+	}
+
+}
