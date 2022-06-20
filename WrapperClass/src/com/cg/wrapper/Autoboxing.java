2e3da717c43b9a15a4fade802bb87a79fa89af93
Aaliya <Aaliya@DESKTOP-3DUR69C>
2022-06-20 16:13:03 -0700

    first commit

---
+++ WrapperClass/src/com/cg/wrapper/Autoboxing.java
@@ -0,0 +1,15 @@
+package com.cg.wrapper;
+
+public class Autoboxing {
+
+	public static void main(String[] args) {
+		int a=20;
+		Integer  i = Integer.valueOf(a);
+		Integer  j=a;
+		System.out.println(a+" "+i+" "+j);
+		
+		
+
+	}
+
+}
